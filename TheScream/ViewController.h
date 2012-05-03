d05607acbaa507ea0aacd94250ddf722dc05e513
T. Andrew Binkowski <abinkowski@uchicago.edu>
2012-05-03 05:12:46 -0500

    Initial Commit

---
+++ TheScream/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TheScream
+//
+//  Created by T. Binkowski on 5/3/12.
+//  Copyright (c) 2012 University of Chicago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
